34124f3c4f3d799237ba4ed6d650da9ce22b73ec
dionation <dionation@gmail.com>
2018-07-03 10:03:35 +0200

    envoi de mon dossier public

---
+++ public/js/main.js
@@ -0,0 +1,18 @@
+var guerrier = {
+  nom : "nico",
+  age : 18,
+  taille : 180,
+  arme : "hache",
+};
+var mage = {
+  nom : "hajar",
+  age : 20,
+  taille : 160,
+  arme : "baton",
+};
+var paysan = {
+  nom : "sylvie",
+  age : 17,
+  taille : 165,
+  arme : "fourche",
+};
