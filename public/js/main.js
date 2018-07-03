3103594a45e5c8bfcdd0e4e3228255e4101afbf7
dionation <dionation@gmail.com>
2018-07-03 10:18:32 +0200

    fin du projet sur les objets et les methods prédéfinis

--- public/js/main.js
+++ public/js/main.js
@@ -16,3 +16,8 @@ var paysan = {
   taille : 165,
   arme : "fourche",
 };
+
+var taille_nom_guerrier = guerrier.nom.length;
+var troiseme_index_arme_guerrier = guerrier.arme.charAt(0);
+var morceau_arme_mage = mage.arme.substr(2,4);
+var fusion_noms_silvie_nico = paysan.nom.concat(guerrier.nom);
